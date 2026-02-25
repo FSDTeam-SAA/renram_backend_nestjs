b4701e6eec38fca7b6f34fcbaf1f37668b5f5ee9
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-02-25 23:14:13 +0600

    user model

--- src/app/module/user/dto/create-user.dto.ts
+++ src/app/module/user/dto/create-user.dto.ts
@@ -1,1 +1,54 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { Date } from 'mongoose';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty({ message: 'First name is required' })
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Last name is required' })
+  lastName: string;
+
+  @IsEmail({}, { message: 'Valid email is required' })
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'Password must be at least 6 characters' })
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  phoneNumber: string;
+
+  @IsString()
+  @IsOptional()
+  bio: string;
+
+  @IsEnum(['user', 'admin'])
+  @IsOptional()
+  role?: string;
+
+  @IsEnum(['male', 'female'])
+  @IsOptional()
+  gender?: string;
+
+  @IsString()
+  @IsOptional()
+  dateOfBirth?: string;
+
+  @IsString()
+  @IsOptional()
+  address: string;
+
+  @IsString()
+  @IsOptional()
+  profilePicture: string;
+}
--- src/app/module/user/entities/user.entity.ts
+++ src/app/module/user/entities/user.entity.ts
@@ -1,1 +1,83 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import config from 'src/app/config';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({
+    required: [true, 'First name is required'],
+    trim: true,
+  })
+  firstName: string;
+
+  @Prop({
+    required: [true, 'Last name is required'],
+    trim: true,
+  })
+  lastName: string;
+
+  @Prop({
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  })
+  email: string;
+
+  @Prop({
+    required: [true, 'Password is required'],
+    minlength: 6,
+    select: false,
+  })
+  password: string;
+
+  @Prop({
+    enum: ['user', 'admin'],
+    default: 'user',
+  })
+  role: string;
+
+  @Prop({ enum: ['male', 'female'] })
+  gender: string;
+
+  @Prop()
+  phoneNumber: string;
+
+  @Prop()
+  bio: string;
+
+  @Prop()
+  profilePicture: string;
+
+  @Prop()
+  dateOfBirth?: Date;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  otp?: string;
+
+  @Prop()
+  otpExpiry?: Date;
+
+  @Prop()
+  verifiedForget: boolean;
+
+  @Prop()
+  stripeAccountId: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+
+  this.password = await bcrypt.hash(
+    this.password,
+    Number(config.bcryptSaltRounds),
+  );
+});
